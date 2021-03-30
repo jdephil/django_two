b9cf2a7ccd89dfc58efee44934af781ea2834fee
jdephil <jennifer.dephillips@gmail.com>
2021-03-30 14:06:23 -0400

    hooked up form to user model

--- AppTwo/views.py
+++ AppTwo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from AppTwo.models import User
+#from django.http import HttpResponse
+from AppTwo.forms import NewUserForm
+#from AppTwo.models import User
 # Create your views here.
 
 
@@ -14,6 +15,20 @@ def help(request):
     return render(request, "AppTwo/help.html", context=my_dict2)
 
 def users(request):
-    users_list = User.objects.order_by('last_name')
-    my_dict3 = {'users_page': users_list}
-    return render(request, "AppTwo/users.html", context=my_dict3)
+  form = NewUserForm()
+
+  if request.method == "POST":
+    form = NewUserForm(request.POST)
+
+    if form.is_valid():
+      form.save(commit=True)
+      return index(request)
+    else:
+      print('error form invalid')
+  
+  return render(request,'AppTwo/users.html', {'form': form})
+
+
+    # users_list = User.objects.order_by('last_name')
+    # my_dict3 = {'users_page': users_list}
+    # return render(request, "AppTwo/users.html", context=my_dict3)
